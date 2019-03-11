fe5ecabd3d8d1ad8ac7aeadc3243436917a19f42
melbied <melbied@norsys.fr>
2019-03-11 16:12:39 +0100

    Creation archetype

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package fr.norsys.technomaker.${artifactId};
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Hello world !");
+    }
+}
