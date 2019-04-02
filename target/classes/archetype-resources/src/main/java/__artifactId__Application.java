e84c46bf89cb6e20de375c8bd2c688c4c225060b
ikram <ibasri>
2019-04-02 18:04:53 +0200

    Replace name with artifactId

--- target/classes/archetype-resources/src/main/java/__artifactId__Application.java
+++ target/classes/archetype-resources/src/main/java/__artifactId__Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ${name}Application
+public class ${artifactId}Application
 {
     public static void main( String[] args )
     {
-    	SpringApplication.run(${name}Application.class, args);
+    	SpringApplication.run(${artifactId}Application.class, args);
     }
 }
